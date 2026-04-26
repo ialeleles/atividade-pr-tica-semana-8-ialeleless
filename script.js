9809c03af01be416ed7be119dc44451981a55bc6
Iale Leles <ialeleles@proton.me>
2026-04-26 17:34:58 -0300

    feat: adiciona uma constante catalogo com array de objetos.

---
+++ script.js
@@ -0,0 +1,60 @@
+// Catálogo
+const catalogo = [
+    {
+        id: 1,
+        titulo: "Breaking Bad",
+        tipo: "serie",
+        ano: 2008,
+        generos: ["ação", "drama"],
+        nota: 9.5,
+        assistido: false 
+    },
+    {
+        id: 2,
+        titulo: "Os Vingadores",
+        tipo: "filme",
+        ano: 2012,
+        generos: ["ação"],
+        nota: 8,
+        assistido: true 
+    },
+    {
+        id: 3,
+        titulo: "Devoradores de Estrelas",
+        tipo: "filme",
+        ano: 2026,
+        generos: ["aventura", "ficção científica"],
+        nota: 8.4,
+        assistido: true 
+    },
+    {
+        id: 4,
+        titulo: "Stranger Things",
+        tipo: "serie",
+        ano: 2016,
+        generos: ["drama", "ficção científica", "terror"],
+        nota: 8.6,
+        assistido: true 
+    },
+    {
+        id: 5,
+        titulo: "Game of Thrones",
+        tipo: "serie",
+        ano: 2011,
+        generos: ["épico", "ação"],
+        nota: 9.2,
+        assistido: false
+    },
+    {
+        id: 6,
+        titulo: "Interestelar",
+        tipo: "filme",
+        ano: 2014,
+        generos: ["aventura", "ficção científica"],
+        nota: 8.7,
+        assistido: true 
+    }
+];
+
+console.log(catalogo)
+
